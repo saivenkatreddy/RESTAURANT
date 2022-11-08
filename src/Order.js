0a219a3591b0b06fa5587aba2b76104b5b8c2b2c
PriyathamVarma <priyatham002@gmail.com>
2022-11-08 19:15:41 +0000

    safe drop from V

---
+++ src/Order.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+const Order = () =>{
+
+
+    console.log('Order component')
+
+    return(
+        <div className="App">
+            <Header/>
+            kjgjkbjklbh
+        </div>
+    )
+}
+
+export default Order;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Order from './Order';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/order",
+    element: <Order/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
